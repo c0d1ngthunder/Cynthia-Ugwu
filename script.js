58ec3c032aec259cb193c0f11475187ff27b628c
Nitish Kumar <codingthunder3@gmail.com>
2025-06-04 14:38:34 +0530

    refactor: refactor code to make it clean

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const locoscroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
-
 var timeout;
-
 function customcursorWithSkew() {
   var xscale = 1;
   var yscale = 1;
@@ -40,79 +34,98 @@ function customcursorWithSkew() {
     }, 100);
   });
 }
+function page2animations() {
+  document.querySelectorAll(".elem").forEach((elem) => {
+    var rotdif = 0;
+    var rotate = 0;
 
-document.querySelectorAll(".elem").forEach((elem) => {
-  var rotdif = 0;
-  var rotate = 0;
+    elem.addEventListener("mouseenter", (dets) => {
+      var cursor = document.querySelector(".cursor");
+      cursor.innerHTML = `VIEW`;
 
-  elem.addEventListener("mouseenter", (dets) => {
-    var cursor = document.querySelector(".cursor");
-    cursor.innerHTML = `VIEW`;
-    
-    gsap.to(cursor, {
-      width: "50px",
-      height: "50px",
-      duration: 0.3,
-      ease: Power3,
+      gsap.to(cursor, {
+        width: "50px",
+        height: "50px",
+        duration: 0.1,
+        ease: Power3,
+      });
+
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        duration: 0.5,
+        ease: Power3,
+      });
+      gsap.to(elem.querySelector(".elemhead"), {
+        x: 50,
+        color: "rgba(255, 255, 255, 0.2)",
+      });
+      gsap.to(elem.querySelector(".year"), {
+        color: "rgba(255, 255, 255, 0.2)",
+      });
     });
 
-    gsap.to(elem.querySelector("img"), {
-      opacity: 1,
-      duration: 0.5,
-      ease: Power3,
+    elem.addEventListener("mousemove", (dets) => {
+      rotdif = dets.clientX - rotate;
+      rotate = dets.clientX;
+      var img = elem.querySelector("img");
+
+      const bounds = elem.getBoundingClientRect();
+      const relX = dets.clientX - bounds.left;
+      const relY = dets.clientY - bounds.top;
+
+      gsap.to(img, {
+        x: relX - img.offsetWidth / 2,
+        y: relY - img.offsetHeight / 3,
+        rotate: gsap.utils.clamp(-20, 20, rotdif),
+        duration: 0.8,
+        ease: Linear,
+      });
     });
-    gsap.to(elem.querySelector(".elemhead"), {
-      x: 80,
-      color: "rgba(255, 255, 255, 0.2)",
-    });
-    gsap.to(elem.querySelector(".year"), {
-      color: "rgba(255, 255, 255, 0.2)",
+
+    elem.addEventListener("mouseleave", (dets) => {
+      var cursor = document.querySelector(".cursor");
+      cursor.innerHTML = ``;
+
+      gsap.to(cursor, {
+        width: "15px",
+        height: "15px",
+        duration: 0.1,
+        ease: Power3,
+      });
+
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        duration: 0.5,
+        ease: Power3,
+      });
+
+      gsap.to(elem.querySelector(".elemhead"), {
+        x: 0,
+        color: "rgba(255, 255, 255, 0.6)",
+      });
+      gsap.to(elem.querySelector(".year"), {
+        color: "rgb(255, 255, 255)",
+      });
     });
   });
+}
+function getTime() {
+  const options = {
+    timeZone: "America/New_York",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  };
 
-  elem.addEventListener("mousemove", (dets) => {
-    rotdif = dets.clientX - rotate;
-    rotate = dets.clientX;
-    var img = elem.querySelector("img");
+  const nowET = new Date().toLocaleString("en-US", options);
+  document.querySelector(".time").innerHTML = nowET + " ET";
+}
 
-    const bounds = elem.getBoundingClientRect();
-    const relX = dets.clientX - bounds.left;
-    const relY = dets.clientY - bounds.top;
-
-    gsap.to(img, {
-      x: relX - img.offsetWidth / 2,
-      y: relY - img.offsetHeight / 3,
-      rotate: gsap.utils.clamp(-20, 20, rotdif),
-      duration: 0.8,
-      ease: Linear,
-    });
-  });
-
-  elem.addEventListener("mouseleave", (dets) => {
-    var cursor = document.querySelector(".cursor");
-    cursor.innerHTML = ``;
-    
-    gsap.to(cursor, {
-      width: "15px",
-      height: "15px",
-      duration: 0.3,
-      ease: Power3,
-    });
-
-    gsap.to(elem.querySelector("img"), {
-      opacity: 0,
-      duration: 0.5,
-      ease: Power3,
-    });
-
-    gsap.to(elem.querySelector(".elemhead"), {
-      x: 0,
-      color: "rgba(255, 255, 255, 0.6)",
-    });
-    gsap.to(elem.querySelector(".year"), {
-      color: "rgb(255, 255, 255)",
-    });
-  });
+const locoscroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
+getTime()
+page2animations();
 customcursorWithSkew();
